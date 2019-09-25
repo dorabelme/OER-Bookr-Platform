e9f3090d65c3ce10cf6ba06600594290d433fb47
Dora Belme <contact.dorabelme@gmail.com>
2019-09-25 16:00:52 -0400

    added seed data to the database

--- database/seeds/03-authors.js
+++ database/seeds/03-authors.js
@@ -1,6 +1,4 @@
 exports.seed = function (knex) {
-    // return knex('authors').truncate()
-    //     .then(function () {
             return knex('authors').insert([
                 { name: 'Lynn Marecek' },                  // 1
                 { name: 'MaryAnne Anthony-Smith' },        // 2
@@ -8,7 +6,26 @@ exports.seed = function (knex) {
                 { name: 'Alexander Holmes' },              // 4
                 { name: 'Barbara Illowsky,' },             // 5
                 { name: 'Susan Dean' },                    // 6
+                { name: 'Glen Krutz' },                    // 7
+                { name: 'Sylvie Waskiewicz' },             // 8
+                { name: 'Steven A. Greenlaw' },            // 9
+                { name: 'David Shapiro' },                 // 10
+                { name: 'Steven A. Greenlaw' },            // 11
+                { name: 'Lawrence J. Gitman' },            // 12
+                { name: 'Carl McDaniel' },                 // 13
+                { name: 'Amit Shah' },                     // 14
+                { name: 'Monique Reece' },                 // 15
+                { name: 'Linda Koffel,' },                 // 16
+                { name: 'Bethann Talsma' },                // 17
+                { name: 'James C. Hyatt' },                // 18
+                { name: 'Michael Laverty' },               // 19
+                { name: 'Chris Littel' },                  // 20
+                { name: 'Mitchell Franklin' },             // 21
+                { name: 'Patty Graybeal' },                // 22
+                { name: 'Dixon Cooper' },                  // 23
+
+
+
         
             ])
-        // })
 }
