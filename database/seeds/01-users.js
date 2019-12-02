42c4a0bb6655018e8ec10b1a6fafa2a2776333fc
Dora Belme <contact.dorabelme@gmail.com>
2019-12-01 21:15:34 -0500

    bcrypt

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,8 +1,8 @@
 const bcrypt = require('bcryptjs');
 
 exports.seed = function (knex) {
-      return knex('users').insert([
-        { username: "admin", password: bcrypt.hashSync("admin1234", 16), name: 'Henry Blevins' },
-        { username: "testuser", password: bcrypt.hashSync("testuser1234", 16), name: 'Elliot Alderson' }
-      ])
+  return knex('users').insert([
+    { username: "admin", password: bcrypt.hashSync("admin1234", 8), name: 'Henry Blevins' },
+    { username: "testuser", password: bcrypt.hashSync("testuser1234", 8), name: 'Elliot Alderson' }
+  ])
 }
